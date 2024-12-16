bdd0dacce4291932f8e6c5310964c7aa466d21ae
kalinplus <13658226+kalinplus@user.noreply.gitee.com>
2024-12-16 10:57:27 +0800

    add multi page_range and users fetching

--- get_user_post.py
+++ get_user_post.py
@@ -1,8 +1,9 @@
 from bs4 import BeautifulSoup
 from datetime import datetime
+import os
 import re
 import requests
-from typing import Any, List
+from typing import Any, List, Tuple
 
 def get_page_user_posts(page_url: str, user_id: str, page: int) -> List[str]:
     '''
@@ -39,22 +40,36 @@ def get_page_user_posts(page_url: str, user_id: str, page: int) -> List[str]:
     # Remove duplicate inital Po主 post
     return posts if (page == 1 or posts == None) else posts[1:]
 
-def save_posts_to_txt(posts: List[str], page: int, filename: str ='No0_0_posts.txt') -> None:
+def save_posts_to_txt(posts: List[str], page: int, user_id: str, filename: str = 'No0_0_posts.txt', save_path: Optional[str] = None) -> None:
     """
     将获取到的一页帖子内容保存到一个文本文件中。
     
     :param posts: 包含一页帖子内容的列表
     :param page: 当前页数
-    :param filename: 输出文件名
+    :param user_id: 用户ID
+    :param filename: 输出文件名，默认为 'No0_0_posts.txt'
+    :param save_path: 保存文件的路径，默认为当前工作目录
     """
-    with open(filename, 'a+', encoding='utf-8') as file:
-        file.write(f"Page {page}\n")
-        for idx, post_content in enumerate(posts, 1):
-            file.write(f"Post {idx}:\n{post_content}\n{'-' * 80}\n")
+    # 构建完整的文件路径
+    if save_path:
+        full_path = os.path.join(save_path, filename)
+    else:
+        full_path = filename
+    
+    # 确保保存路径存在
+    os.makedirs(os.path.dirname(full_path), exist_ok=True)
 
-    print(f"Save page {page} successfully")
+    with open(full_path, 'a+', encoding='utf-8') as file:
+        file.write(f"Page {page}    Poster {user_id}\n\n\n")
+        if not posts:  # 检查posts是否为空或None
+            file.write("No post")
+        else:
+            for idx, post_content in enumerate(posts, 1):
+                file.write(f"Post {idx}:\n{post_content}\n{'-' * 80}\n")
+    
+    print(f"Save page {page} successfully to {full_path}")
 
-def save_all_posts_given_base_url(base_url: str, page_start: int, page_end: int, user_id: str) -> None:
+def save_all_posts_given_base_url(base_url: str, page_range: Tuple[int, int], user_id: str) -> None:
     '''
     Get the posts the given user, for all pages of the entire thread, then save it to text file
     Because the feacture of x岛, each page will contains the initial post,
@@ -62,53 +77,55 @@ def save_all_posts_given_base_url(base_url: str, page_start: int, page_end: int,
     @param base_url: the url of the thread, without the page part
     '''
     url = base_url + "?page={}"
-    pages_to_check = range(page_start, page_end + 1)
+    pages_to_check = range(page_range[0], page_range[1] + 1)
     thread_number = base_url.split('/')[-1]
 
     # Name file with current time to avoid duplication
     current_time = datetime.now().strftime("%Y%m%d%H%M%S")
     filename = 'No' + thread_number + '_' + user_id + "_posts_" + current_time + ".txt"
 
+    print(f"Fetching posts of {user_id}...\n")
     for page in pages_to_check:
         page_url = url.format(page)
         posts = get_page_user_posts(page_url, user_id, page)
         
         print(f"Fetch page {page} posts successfully")
-        save_posts_to_txt(posts, page, filename)
+        save_posts_to_txt(posts=posts, page=page, user_id=user_id, filename=filename)
 
-def save_all_posts_given_thread_number(thread_number: str, page_start: int, page_end: int, user_id: str) -> None:
+def save_all_posts_given_thread_number(thread_number: str, page_range: Tuple[int, int], user_id: str) -> None:
     '''
     @param thread_number: 串号，预期只有数字
     '''
     base_url = 'https://www.nmbxd1.com/t/' + thread_number
-    save_all_posts_given_base_url(base_url, page_start, page_end, user_id) 
+    save_all_posts_given_base_url(base_url, page_range, user_id) 
 
-def save_all_posts(user_input: str, page_start: int, page_end: int, user_id: str) -> None:
+def save_all_posts_single_user(user_input: str, page_range: Tuple[int, int], user_id: str) -> None:
     '''
-    @param user_input: 预期接受url(不包含page), No.串号和纯数字串号三种格式
+    @param user_input: 预期接受url(不包含page), No.串号, 纯数字串号三种格式
     ''' 
     url_pattern = r'^https://www\.nmbxd1\.com/t/\d+$'
     no_pattern = r'^No\.\d+$'
     num_pattern = r'^\d+$'
+
     if re.match(url_pattern, user_input):
-        save_all_posts_given_base_url(user_input, page_start, page_end, user_id)
+        save_all_posts_given_base_url(user_input, page_range, user_id)
     elif re.match(no_pattern, user_input):
         thread_number = user_input.split('.')[1]
-        save_all_posts_given_thread_number(thread_number, page_start, page_end, user_id)
+        save_all_posts_given_thread_number(thread_number, page_range, user_id)
     elif re.match(num_pattern, user_input):
-        save_all_posts_given_thread_number(user_input, page_start, page_end, user_id)
+        save_all_posts_given_thread_number(user_input, page_range, user_id)
     else:
         print(f"Error: Input '{user_input}' does not match any expected format.")
         return None
 
-    print("Done")
+    print("Done\n")
 
-# def save_all_posts_multi_users(user_input: str, page_starts: List[int], page_ends: List[int], user_ids: List[str]) -> None:
-    # for (page_start, page_end, user_id) in zip(page_starts, page_ends, user_ids):
+def save_all_posts_multi_users(user_input: str, page_ranges: List[Tuple[int, int]], user_ids: List[str]) -> None:
+    for (page_range, user_id) in zip(page_ranges, user_ids):
+        save_all_posts_single_user(user_input, page_range, user_id)
          
 if __name__ == "__main__":
     base_url = 'https://www.nmbxd1.com/t/60184882'
-    page_start = 1
-    page_end = 14
-    user_id = 'fyBPEP5'
-    save_all_posts(base_url, page_start, page_end, user_id)
+    page_ranges = [(1, 8), (8, 14)]
+    user_ids = ['fyBPEP5', 'LhWkRsO']
+    save_all_posts_multi_users(user_input=base_url, page_ranges=page_ranges, user_ids=user_ids)
